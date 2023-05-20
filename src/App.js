01f50c100074bebddf25c148a69cf3f31b710d59
pengqiang.li <pengqiang.li@accenture.com>
2023-05-21 01:04:36 +0800

    home page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     
     <>
-      <BrowserRouter>
+      <BrowserRouter basename='agented'>
       <Link to='/'>Home</Link>
       <Link to='about'>About</Link>
       
